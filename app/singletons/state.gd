c9829119ea32f1fbe4eabdb83318f620d5121735
Andrei Achim <andrei.bogdanel.achim@gmail.com>
2024-04-12 18:24:20 +0300

    config for testing app on android

--- app/singletons/state.gd
+++ app/singletons/state.gd
@@ -7,4 +7,4 @@ func _ready():
 	var base_url_env_var = OS.get_environment("cookaid_base_url")
 	API_BASE_URL = base_url_env_var \
 		if base_url_env_var.length() > 0 \
-		else "http://localhost:8080"
+		else "http://192.168.0.129:8080"
